833f8897456d1c6b09ec79753ebf6992540a82b5
shailasiva <shailaja.venkatarama@gmail.com>
2016-07-10 14:01:16 +0530

    added one more sop
    added one more sop

--- src/QSP/DemoA.java
+++ src/QSP/DemoA.java
@@ -4,7 +4,9 @@ public class DemoA {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
- System.out.println("First time GIT hub");
+ System.out.println("First time GIT hub ");
+  System.out.println("WOW ");
+ 
 	}
 
 }
